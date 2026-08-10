ef946987f82940584aa5f21076f50bb0b6472894
x2 <lunguj665@gmail.com>
2026-08-10 15:34:44 +0200

    added the fp videojs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 let x = 10
 let y = "20s;gljdlgjdlkghklfhglfdjgfkldgjdfkljgkldjgldk"
-let sum = x + y
 let a = true
-console.log(10 - 10)
-
-//fumctios ... pure fumctions and side effects
+//console.log(10 - 10)
+//functios ... pure fumctions and side effects
+const name = "john"
+function print(input) {
+  console.log(input)
+  return
+}
+const add = function(x, y) {
+  return x + y
+}
+const double = (x) => {
+  return 2 * x
+}
+const sum = add(double(6), 10)
+print(sum)
+//purity
+//
+//
+//
+//
+//
+function changeName(Name) {
+  let n = name
+  let newName = n + Name
+  return newName
+}
+function sayName() {
+  return name
+}
+function doSomething() {
+  let newName = changeName("peter")
+  print(newName)
+  //.....
+  print(sayName())
+}
+doSomething()
